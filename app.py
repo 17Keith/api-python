777e36c3d704b2c532490a07228f063c8aa572d0
17Keith <kayomega7@gmail.com>
2024-05-24 10:11:40 +0300

    Finish the CRUD api

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
 tasks = [
-
-
     {
         'id': 1,
         'title': 'Random things for today!',
         'description': 'Python, JS',
         'done': False
-
     },
     {
         'id': 2,
@@ -20,27 +17,73 @@ tasks = [
         'description': 'Python, JS',
         'done': False
     }
-
 ]
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def index():
-    # return "I hope it is working"
+def get_tasks():
     return jsonify({'tasks': tasks})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': task})
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or 'title' not in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1 if tasks else 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and not isinstance(request.json['title'], str):
+        abort(400)
+    if 'description' in request.json and not isinstance(request.json['description'], str):
+        abort(400)
+    if 'done' in request.json and not isinstance(request.json['done'], bool):
+        abort(400)
+
+    task['title'] = request.json.get('title', task['title'])
+    task['description'] = request.json.get('description', task['description'])
+    task['done'] = request.json.get('done', task['done'])
+    return jsonify({'task': task})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task is None:
+        abort(404)
+    tasks.remove(task)
+    return jsonify({'result': True})
 
 
 if __name__ == "__main__":
